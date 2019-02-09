183f3c103a7e62bc619207fdfb521e0bdbbcf1f6
Mihai Potra <mike@mpotra.com>
2019-02-09 13:32:21 +0200

    src: Use Attribute List helper inside Element

--- dom/Element.js
+++ dom/Element.js
@@ -2,11 +2,13 @@ const Node = require('./Node');
 const {ELEMENT_NODE} = require('./node-types');
 const {SET_NODE_TYPE} = require('./helpers/node');
 const {getNamespace, getNamespacePrefix, getLocalName, getElementHTMLQualifiedName} = require('./helpers/namespace');
+const AttributeList = require('./helpers/attribute-list');
 
 class Element extends Node {
   constructor() {
     super();
     SET_NODE_TYPE(ELEMENT_NODE);
+    this[kAttributeList] = new AttributeList(this);
   }
 
   get namespaceURI() {
