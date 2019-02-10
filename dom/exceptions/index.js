c8c6cc67333d022b180a38777d3f937cd9084208
Mihai Potra <mike@mpotra.com>
2019-02-10 09:34:33 +0200

    Add InUseAttributeError exception

--- dom/exceptions/index.js
+++ dom/exceptions/index.js
@@ -1,9 +1,11 @@
 const HierarchyRequestError = require('./HierarchyRequestError');
 const NotFoundError = require('./NotFoundError');
 const NotSupportedError = require('./NotSupportedError');
+const InUseAttributeError = require('./InUseAttributeError');
 
 module.exports = {
   HierarchyRequestError,
   NotFoundError,
-  NotSupportedError
+  NotSupportedError,
+  InUseAttributeError
 };
