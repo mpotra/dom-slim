611042106a097112bd9db204a458d0aad569356f
Mihai Potra <mike@mpotra.com>
2019-02-15 15:02:57 +0200

    Add InvalidCharacterError and NamespaceError

--- dom/exceptions/index.js
+++ dom/exceptions/index.js
@@ -2,10 +2,14 @@ const HierarchyRequestError = require('./HierarchyRequestError');
 const NotFoundError = require('./NotFoundError');
 const NotSupportedError = require('./NotSupportedError');
 const InUseAttributeError = require('./InUseAttributeError');
+const InvalidCharacterError = require('./InvalidCharacterError');
+const NamespaceError = require('./NamespaceError');
 
 module.exports = {
   HierarchyRequestError,
   NotFoundError,
   NotSupportedError,
-  InUseAttributeError
+  InUseAttributeError,
+  InvalidCharacterError,
+  NamespaceError
 };
