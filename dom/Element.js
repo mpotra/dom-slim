d438a40b0f28f4822e947ce3da21f22a71c0b04d
Mihai Potra <mike@mpotra.com>
2019-02-15 15:08:28 +0200

    Add attributes handling in DOM Element interface

--- dom/Element.js
+++ dom/Element.js
@@ -1,14 +1,37 @@
 const Node = require('./Node');
+const NamedNodeMap = require('./NamedNodeMap');
 const {ELEMENT_NODE} = require('./node-types');
+const {kAttributeList, kElementAttributes} = require('./symbols');
 const {SET_NODE_TYPE} = require('./helpers/node');
-const {getNamespace, getNamespacePrefix, getLocalName, getElementHTMLQualifiedName} = require('./helpers/namespace');
+const {
+  getNamespace, getNamespacePrefix, getLocalName, getElementHTMLQualifiedName, getQualifiedName
+} = require('./helpers/namespace');
+const {
+  getAttribute,
+  getAttributeNS,
+  hasAttribute,
+  hasAttributeNS,
+  removeAttribute,
+  removeAttributeNS,
+  setAttribute,
+  setAttributeNS,
+  toggleAttribute
+} = require('./helpers/element');
 const AttributeList = require('./helpers/attribute-list');
+const {setElement: setNamedNodeMapElement} = require('./helpers/named-node-map');
+
 
 class Element extends Node {
   constructor() {
     super();
     SET_NODE_TYPE(ELEMENT_NODE);
-    this[kAttributeList] = new AttributeList(this);
+    this[kAttributeList] = new AttributeList();
+    this[kElementAttributes] = new NamedNodeMap();
+    setNamedNodeMapElement(this[kElementAttributes], this);
+  }
+
+  get attributes() {
+    return this[kElementAttributes];
   }
 
   get namespaceURI() {
@@ -29,6 +52,56 @@ class Element extends Node {
 
   getElementsByTagName(tag) {
     return this.childNodes.filter((node) => node.nodeValue === tag);
+  }
+
+  hasAttribute(qualifiedName) {
+    return hasAttribute(this, qualifiedName);
+  }
+
+  hasAttributes() {
+    return (this[kAttributeList].length > 0);
+  }
+
+  hasAttributeNS(namespace, localName) {
+    if (namespace === '') {
+      namespace = null;
+    }
+
+    return hasAttributeNS(this, namespace, localName);
+  }
+
+  getAttributeNames() {
+    return Array.from(this[kAttributeList]).map(getQualifiedName);
+  }
+
+  getAttribute(qualifiedName) {
+    const attr = getAttribute(this, qualifiedName);
+    return (attr ? attr.value : null);
+  }
+
+  getAttributeNS(namespace, localName) {
+    const attr = getAttributeNS(this, namespace, localName);
+    return (attr ? attr.value : null);
+  }
+
+  setAttribute(qualifiedName, value) {
+    setAttribute(this, qualifiedName, value);
+  }
+
+  setAttributeNS(namespace, qualifiedName, value) {
+    setAttributeNS(this, namespace, qualifiedName, value);
+  }
+
+  removeAttribute(qualifiedName) {
+    removeAttribute(this, qualifiedName);
+  }
+
+  removeAttributeNS(namespace, localName) {
+    return removeAttributeNS(this, namespace, localName);
+  }
+
+  toggleAttribute(qualifiedName, force) {
+    return toggleAttribute(this, qualifiedName, force);
   }
 }
 
