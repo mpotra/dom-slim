dc10fedbd0ee9a957c3e6042ac1ed1f973d3f3c1
Mihai Potra <mike@mpotra.com>
2019-02-18 12:51:09 +0200

    Expose DOM interfaces

--- dom/index.js
+++ dom/index.js
@@ -1,19 +1,33 @@
 const Node = require('./Node');
 const Document = require('./Document');
 const Element = require('./Element');
+const Attr = require('./Attr');
 const CharacterData = require('./CharacterData');
 const Text = require('./Text');
-const Context = require('./Context');
+const Comment = require('./Comment');
+const CDATASection = require('./CDATASection');
+const ProcessingInstruction = require('./ProcessingInstruction');
+const NamedNodeMap = require('./NamedNodeMap');
 const CustomElementRegistry = require('./CustomElementRegistry');
 const NodeList = require('./NodeList');
+const DOMException = require('./DOMException');
+const DocumentFragment = require('./DocumentFragment');
+const DocumentType = require('./DocumentType');
 
 module.exports = {
   Node,
   Document,
   Element,
+  Attr,
   Text,
   CharacterData,
-  Context,
+  Comment,
+  CDATASection,
+  ProcessingInstruction,
   CustomElementRegistry,
-  NodeList
+  NamedNodeMap,
+  NodeList,
+  DOMException,
+  DocumentFragment,
+  DocumentType
 };
