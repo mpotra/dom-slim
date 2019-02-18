ab0dedc28a884348641c62e35dc9a60ceb775112
Mihai Potra <mike@mpotra.com>
2019-02-18 10:07:16 +0200

    Set node type on DocumentType interface

--- dom/DocumentType.js
+++ dom/DocumentType.js
@@ -1,9 +1,12 @@
 const Node = require('./Node');
+const {DOCUMENT_TYPE_NODE} = require('./node-types');
+const {SET_NODE_TYPE} = require('./helpers/node');
 const {getName, getPublicId, getSystemId} = require('./helpers/document-type');
 
 class DocumentType extends Node {
   constructor() {
     super();
+    SET_NODE_TYPE(this, DOCUMENT_TYPE_NODE);
   }
 
   get name() {
